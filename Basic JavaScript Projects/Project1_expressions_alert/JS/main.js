7e0faef5cdbfa3baebd67d765e6975be704ca683
ncory44 <ncory44@gmail.com>
2025-02-05 15:32:30 -0800

    Starting Basic JavaScript

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+window.alert("Hello, World!");
+
+document.write("Hello, World! <br>");  //Writes the string "Hello, World!"
+
+var a = "This is a string";            
+
+document.write(a);                     //Writes the string of variable a
+
+document.write("The cat said, \"meow\" <br>")
+
+var B = "This is a statement"
+    + "that has been concatenated <br>";
+
+document.write(B);                     //Writes the variable B concatenated
+
+var Student = "Jon", Teacher = "Alex" , Headmaster = "Sara";
+
+document.write(Teacher);               //Writes the variable Teacher as "alex"
+
+document.write(2+2);                   //Solves 2+2, writes 4
+
